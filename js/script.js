f68bf92ca123b89d44f440c80e49bad411322e56
vsheva <valera.sheva@gmail.com>
2023-10-22 23:25:33 -0400

    movie detail

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const global = {
 
 const showMoviePopular = async () => {
   let { results } = await fetchApi('movie/popular');
-  console.log(results);
 
   results.map(movie => {
     const div = document.createElement('div');
@@ -20,7 +19,7 @@ const showMoviePopular = async () => {
         ${
           movie.poster_path
             ? `
-        <img
+     <img
         src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
         class="card-img-top"
         alt=${movie.title}
@@ -47,12 +46,115 @@ const showMoviePopular = async () => {
   });
 };
 
+const showShowsPopular = async () => {
+  let { results } = await fetchApi('tv/popular');
+
+  results.map(show => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `
+     <img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt=${show.name}
+      />
+         `
+            : `  <img
+         src="images/no-image.jpg"
+         class="card-img-top"
+         alt="${show.name}"
+       />
+         `
+        }
+          </a>
+  
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Air Date: ${show.first_air_date}</small>
+      </p>
+      </div>
+    `;
+
+    document.getElementById('popular-shows').appendChild(div);
+  });
+};
+
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split('=')[1]; // ?id=299054
+
+  const movie = await fetchApi(`movie/${movieId}`);
+
+  console.log(movie);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+    <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `
+       <img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt=${movie.title}
+        />
+           `
+              : `  <img
+           src="images/no-image.jpg"
+           class="card-img-top"
+           alt="${movie.title}"
+         />
+           `
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')} //!!!
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
+            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>
+    `;
+
+  document.getElementById('movie-details').appendChild(div);
+};
+
 const fetchApi = async end => {
   const API_KEY = '668a01fc8852e1c4fe98bb335096d80a';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
   const response = await fetch(`${API_URL}${end}?api_key=${API_KEY}&language=en-US`);
   const data = await response.json();
+  hideSpinner();
   return data;
 };
 
@@ -68,6 +170,14 @@ const highlightActiveLink = () => {
   });
 };
 
+const showSpinner = () => {
+  document.querySelector('.spinner').classList.add('show');
+};
+
+const hideSpinner = () => {
+  document.querySelector('.spinner').classList.remove('show');
+};
+
 const init = () => {
   switch (global.currentPage) {
     case '/':
@@ -75,10 +185,10 @@ const init = () => {
       showMoviePopular();
       break;
     case '/shows.html':
-      console.log('Shows');
+      showShowsPopular();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
