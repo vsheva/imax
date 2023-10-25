da46f46dc21846a4f492b6311d36418e86e78f73
vsheva <valera.sheva@gmail.com>
2023-10-24 22:52:18 -0400

    fix errors 24okt 22:54

--- js/script.js
+++ js/script.js
@@ -257,53 +257,52 @@ const fetchApi = async end => {
   return data;
 };
 
-//slider swiper for Movies
-// const displaySlider = async () => {
-//   const { results } = await fetchApi('movie/now_playing');
-//   console.log(results);
+//slider slider swiper for Movies
+const displaySlider = async () => {
+  const { results } = await fetchApi('movie/now_playing');
+  console.log(results);
 
-//   results.map(movie => {
-//     const div = document.createElement('div');
-//     div.classList.add('swiper-slide');
-//     div.innerHTML = `
-//   <div class="swiper-slide">
-//   <a href="movie-details.html?id=${movie.is}">
-//     <img src="https://tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
-//   </a>
-//   <h4 class="swiper-rating">
-//     <i class="fas fa-star text-secondary"></i> ${movie.vote_average} /10
-//   </h4>
-//   </div>
-//   `;
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
 
-//     document.querySelector('.swiper-wrapper').appendChild(div);
-//     initSwiper();
-//   });
-// };
+    div.innerHTML = `
+  <a href="movie-details.html?id=${movie.id}">
+    <img src="https://tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+  </a>
+  <h4 class="swiper-rating">
+    <i class="fas fa-star text-secondary"></i> ${movie.vote_average} /10
+  </h4>
+  `;
 
-// const initSwiper = () => {
-//   const swiper = new Swiper('.swiper', {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     freeMode: true,
-//     loop: true,
-//     autoplay: {
-//       delay: 4000,
-//       disableOnInteraction: false,
-//     },
-//     breakpoints: {
-//       500: {
-//         slidesPerView: 2,
-//       },
-//       700: {
-//         slidesPerView: 3,
-//       },
-//       1200: {
-//         slidesPerView: 4,
-//       },
-//     },
-//   });
-// };
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    initSwiper();
+  });
+};
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
 
 // Highlight active link
 
@@ -333,7 +332,7 @@ const init = () => {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      // displaySlider();
+      displaySlider();
       showMoviePopular();
       break;
     case '/shows.html':
